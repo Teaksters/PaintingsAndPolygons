aeb6db3b1de3f58ac8a95420bc2621ac06490644
Teaksters <arneb.meijs@gmail.com>
2019-12-03 11:43:37 +0100

    going to make changes to scaling and where I do it

--- analysis.py
+++ analysis.py
@@ -2,11 +2,16 @@ import pandas as pd
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
 # Parameters to set the plot type
+N_paintings = 9
 singular = False
-multiple_V = True
+multiple_V = False
 multiple_P = False
+scatter_S = False
+scatter_M_gen = True
+scatter_M_Vgen = False
 
 
 def read_data_file(fp):
@@ -59,7 +64,40 @@ def singular_plot(V_p, MSE_scores, MSE_err, painting, V_tot):
     plt.title(title)
     plt.xlabel('Vertices Per Polygon')
     plt.ylabel('% Offset of mean MSE')
-    fig_fp = os.path.join('Analysis', 'singular',
+    fig_fp = os.path.join('Analysis', 'singular', 'Mean',
+                          painting + "_V_" + str(V_tot) + '.png')
+    plt.savefig(fig_fp)
+    plt.close()
+    return fig
+
+def scatter_plot(x, y, label):
+    plt.scatter(x, y, marker='x', alpha=0.5, label=label)
+    plt.xticks(x, x)
+
+
+def singular_fitted_scatter_plot(V_p, MSE_val, painting, V_tot):
+    # Reorganize data for plotting
+    x = np.array([V_p[i] for i in range(len(MSE_val)) for j in range(len(MSE_val[i]))])
+    y = np.array(MSE_val).flatten()
+
+    # Create a regression line model of data
+    X, Y = x.reshape(-1, 1), y.reshape(-1, 1)
+    regr = LinearRegression()
+    regr.fit(X, Y)
+    X_uniq = np.unique(x).reshape(-1, 1)
+    Y_pred = regr.predict(X_uniq)
+    X_uniq, Y_pred = X_uniq.flatten(), Y_pred.flatten()
+
+    # Plot the scatter and fitted line
+    fig = plt.figure()
+    scatter_plot(x, y, 'Data points')
+    plt.plot(X_uniq, Y_pred, color='k', label='trend line')
+    title = painting + '_total vertices_' + str(V_tot)
+    plt.legend(loc='upper left')
+    plt.title(title)
+    plt.xlabel('Vertices Per Polygon')
+    plt.ylabel('% Offset of mean MSE')
+    fig_fp = os.path.join('Analysis', 'singular', 'scatter_Lfit',
                           painting + "_V_" + str(V_tot) + '.png')
     plt.savefig(fig_fp)
     plt.close()
@@ -79,6 +117,82 @@ def multiple_V_plot(V_p, MSE_means, MSE_errs, painting, V_tots):
     fig_fp = os.path.join('Analysis', 'vertices', painting + "_All_Vertici.png")
     plt.savefig(fig_fp)
     plt.close()
+    return fig
+
+
+def multiple_scatterfit_gen_plot(V_p, MSE_vals, painting):
+    # Reorganize data for plotting
+    MSE_val = np.array([[MSE_vals[i][j] for i in range(len(MSE_vals))]
+                        for j in range(4)])
+    MSE_val = MSE_val.reshape(4, -1)
+    y_std = np.std(MSE_val, axis=1)
+    y_mean = np.mean(MSE_val, axis=1)
+    x = np.array([V_p[i] for i in range(len(MSE_val)) for j in range(len(MSE_val[i]))])
+    y = MSE_val.flatten()
+
+    # Create a regression line model of data
+    X, Y = x.reshape(-1, 1), y.reshape(-1, 1)
+    regr = LinearRegression()
+    regr.fit(X, Y)
+    X_uniq = np.unique(x).reshape(-1, 1)
+    Y_pred = regr.predict(X_uniq)
+    X_uniq, Y_pred = X_uniq.flatten(), Y_pred.flatten()
+
+    # Plot the scatter and fitted line
+    fig = plt.figure()
+    scatter_plot(x, y, 'Data points')
+    plt.plot(X_uniq, Y_pred, color='k', label='trend line')
+    # plt.plot(X_uniq, y_mean, color='blue', label='mean')  # If including mean
+    plt.fill_between(X_uniq, y_mean - y_std, y_mean + y_std, color='blue', alpha=0.3, label='standard deviation')
+    title = 'All_paintings_General_trend_V'
+    plt.legend(loc='upper left')
+    plt.title(title)
+    plt.xlabel('Vertices Per Polygon')
+    plt.ylabel('MSE / max_MSE')
+
+    # set axes range
+    # plt.yticks(np.arange(0.0, 1.0, 0.1))
+    plt.grid(axis='y')
+    fig_fp = os.path.join('Analysis', 'General', 'scatter_Lfit_Pgen', 'max',
+                          painting + '.png')
+    plt.savefig(fig_fp)
+    plt.close()
+    return fig
+
+
+def multiple_scatterfit_GEN_plot(V_p, MSE_vals, V_tots):
+    # Reorganize data for plotting
+    offset = N_paintings - 1
+    temp = [[MSE_vals[i + (offset * j)] for i in range(len(V_tots))] for j in range(N_paintings)]
+    for i in range(len(temp)):
+        MSE_val = np.array([[temp[i][j][k] for j in range(len(temp[i]))]
+                            for k in range(4)])
+        MSE_val = MSE_val.reshape(4, -1)
+        MSE_val = MSE_val.reshape(4, -1)
+        x = np.array([V_p[i] for i in range(len(MSE_val)) for j in range(len(MSE_val[i]))])
+        y = MSE_val.flatten()
+
+        # Create a regression line model of data
+        X, Y = x.reshape(-1, 1), y.reshape(-1, 1)
+        regr = LinearRegression()
+        regr.fit(X, Y)
+        X_uniq = np.unique(x).reshape(-1, 1)
+        Y_pred = regr.predict(X_uniq)
+        X_uniq, Y_pred = X_uniq.flatten(), Y_pred.flatten()
+
+        # Plot the scatter and fitted line
+        fig = plt.figure()
+        scatter_plot(x, y, 'Data points')
+        plt.plot(X_uniq, Y_pred, color='k', label='trend line')
+        title = 'General trend all paintings for ' + str(V_tots[i % len(V_tots)]) + ' Vertices'
+        plt.legend(loc='upper left')
+        plt.title(title)
+        plt.xlabel('Vertices Per Polygon')
+        plt.ylabel('% Offset of mean MSE')
+        fig_fp = os.path.join('Analysis', 'General', 'scatter_Lfit_Pgen',
+                              str(V_tots[i % len(V_tots)]) + '.png')
+        plt.savefig(fig_fp)
+        plt.close()
     return fig
 
 
@@ -106,6 +220,7 @@ def multiple_P_plot(V_p, MSE_means, MSE_errs, paintings, V_tots):
 if __name__ == "__main__":
     multiple_means = []
     multiple_std = []
+    MSE_scaled_all = []
     V_p = []
     V_tot = []
     datafp = os.path.join("Results", "HC-DATA.csv")
@@ -119,26 +234,22 @@ if __name__ == "__main__":
         for j in range(len(V_tot)):
             # Per painting and vertices used split on vertices per polygon
             dfs_vtot[j] = dfs_vtot[j].sort_values(by=['Vertices Per Polygon'])
-            dfs_Vp, V_p = split_df(dfs_vtot[j], 'Vertices Per Polygon')
 
+            dfs_Vp, V_p = split_df(dfs_vtot[j], 'Vertices Per Polygon')
             # Format data to plottable state
             sample_MSE_list = [sample["MSE"].to_list() for sample in dfs_Vp]
             sample_MSE_list = np.array(sample_MSE_list)
 
-            # Scale for comparison purposes
-            all_mean = np.mean(sample_MSE_list)
-            MSE_scaled = (sample_MSE_list - all_mean) / all_mean
-
-            # calculate mean and standard deviation
-            MSE_std = np.std(MSE_scaled, axis=1)
-            MSE_mean = np.mean(MSE_scaled, axis=1)
-
             if singular:
                 singular_plot(V_p, MSE_mean, MSE_std, paintings[i], V_tot[j])
-
             if multiple_P or multiple_V:
                 multiple_means.append(MSE_mean)
                 multiple_std.append(MSE_std)
+
+            if scatter_S:
+                singular_fitted_scatter_plot(V_p, MSE_scaled, paintings[i], V_tot[j])
+            else:
+                MSE_scaled_all.append(MSE_scaled)
 
         # Plot multiple V_tot per painting
         if multiple_V:
@@ -147,12 +258,19 @@ if __name__ == "__main__":
             multiple_means = []
             multiple_std = []
 
+        if scatter_M_gen:
+            multiple_scatterfit_gen_plot(V_p, MSE_scaled_all, paintings[i])
+            MSE_scaled_all = []
+
+    if scatter_M_Vgen:
+        multiple_scatterfit_GEN_plot(V_p, MSE_scaled_all, V_tot)
+        MSE_scaled_all = []
+
     if multiple_P:
         multiple_P_plot(V_p, multiple_means, multiple_std, paintings,
                         V_tot)
         multiple_means = []
         multiple_std = []
-
 
 
 
